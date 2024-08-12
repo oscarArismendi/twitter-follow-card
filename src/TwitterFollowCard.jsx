232fed2d23c174a25994478faea7150c28427a54
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-08-11 23:02:38 -0500

    feat: implement TwitterFollowCard component with follow/unfollow functionality

---
+++ src/TwitterFollowCard.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export function TwitterFollowCard({children,formatUserName,userName = "unknown",initialIsFollowing }){
+    const [isFollowing,setIsFollowing] = useState(initialIsFollowing );
+    
+    const imageSrc = `https://unavatar.io/${userName}`;
+    const text = isFollowing ? 'Following' : "Follow";
+    const buttonClassName = isFollowing ? "tw-followCard-button is-following" : "tw-followCard-button";
+    
+    const handleClick = ()=>{
+        setIsFollowing(!isFollowing);
+    }
+    
+    return (
+        <article className="tw-followCard">
+            <header className="tw-followCard-header">
+                <img className="tw-followCard-avatar" src={imageSrc} alt="Avatar of the user" />
+                <div className="tw-followCard-info">
+                    <strong>{children}</strong>
+                    <span className="tw-followCard-infoUserName">{formatUserName(userName)}</span>
+                </div>
+            </header>
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Unfollow</span>
+                </button>
+            </aside>
+        </article>
+    )
+}
