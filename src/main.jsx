9d1ddc98cae783191981728fd3212f9750100c38
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-08-11 23:00:26 -0500

    feat: initialize React application with App component

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import { createRoot } from 'react-dom/client'
+import React from 'react'
+import { App } from './App'
+import "./index.css"
+createRoot(document.getElementById('root')).render(
+  <React.Fragment>
+    <App />
+  </React.Fragment>
+
+)
